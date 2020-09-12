08bbf447828820c2eeb0b67771a37d188d8d9c9f
unknown <ashrithapuradamane@gmail.com>
2020-09-11 19:42:57 -0500

    Updates

---
+++ precache-manifest.ba7f9aef757ddd6332b36b7c3da1004a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f59e4e6012ec6a0f8bc548e40cae8888",
+    "url": "/React-Js-Job-portal/index.html"
+  },
+  {
+    "revision": "7a230c581b95aec0e511",
+    "url": "/React-Js-Job-portal/static/css/main.2273eb1e.chunk.css"
+  },
+  {
+    "revision": "90aac834147ec266f39f",
+    "url": "/React-Js-Job-portal/static/js/2.82bd41c6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-Js-Job-portal/static/js/2.82bd41c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a230c581b95aec0e511",
+    "url": "/React-Js-Job-portal/static/js/main.098137d6.chunk.js"
+  },
+  {
+    "revision": "bc3abfe46fe36276509a",
+    "url": "/React-Js-Job-portal/static/js/runtime-main.a062f3a8.js"
+  }
+]);
